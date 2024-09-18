bb523df796d468c49018a990cd7ff5d1d6e408b2
megs9475 <mlongua@uwyo.edu>
2024-09-18 21:30:56 +0000

    updated name,etc.

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Meghan Longua
 # UWYO COSC 1010
-# Submission Date
+# Submission Date: 9/18/2024
 # Lab 02 
-# Lab Section: 
+# Lab Section:15 
 # Sources, people worked with, help given to: 
 # your
 # comments
